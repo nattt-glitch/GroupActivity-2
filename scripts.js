36f36af7eab4e02a12ed1dec5dbc0bb6fd7e43b4
nattt-glitch <nathnhate@gmail.com>
2025-02-23 02:27:34 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("startButton").addEventListener("click", function () {
+    window.location.href = "terminologies.html";
+});
+
+document.getElementById("backButton").addEventListener("click", function () {
+    window.location.href = "index.html";
+});
